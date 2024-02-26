b212c62f1a83c5a7e129f931e12ddbe5dd2e5bd1
Jamie Lemon <107279992+jamie-lemon@users.noreply.github.com>
2024-02-26 15:08:25 +0000

    Node Server & NextJS example (#10)
    * Adds basic client/server setup with NextJS and Node.
    
    * Updates NodeJS project with new test.pdf and renames base node server API URL stub.
    
    * Adds more to Node Server & NextJS demo
    
    - Adds some styling, comments and file cleanup to Next JS example.
    - Adds notes to node server.js.
    - Adds test.js for testing directly with Node on
    command line.
    
    * Removes the internal API stuff from node server.js
    
    - Because we can just use higher level API with
    page.toPixmap

---
+++ examples/mupdf-nextjs/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Main from './Main'
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "MuPDF Next App",
+  description: "A sample NextJS app",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Main />
+      </body>
+    </html>
+  );
+}
---
+++ node-server/README.md
@@ -0,0 +1,29 @@
+# A Node Server for MuPDF.js
+
+Install:
+
+```bash
+npm install
+```
+
+Start the server:
+
+```bash
+npm run dev
+```
+
+## Test the server
+
+Visit: http://localhost:8080/mupdfjs/
+
+You should see the "Hello World!" message.
+
+
+# Just want to test API directly with Node.JS ?
+
+Please do:
+
+```bash
+node test.js
+```
+

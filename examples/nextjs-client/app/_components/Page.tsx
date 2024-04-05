ddd3d0cdc31c87f5ff56ffe83cef3e0e18f1655b
Yuto Takahashi <mipo1357@gmail.com>
2024-04-05 20:50:19 +0900

    fix type error

--- examples/nextjs-client/app/_components/Page.tsx
+++ examples/nextjs-client/app/_components/Page.tsx
@@ -5,10 +5,8 @@ import { useEffect, useRef, useState } from "react";
 export type SearchResult = {
   page: number;
   results: {
-    x: number;
-    y: number;
-    w: number;
-    h: number;
+    bbox: { x: number; y: number; w: number; h: number };
+    text: string;
   }[];
   pageWidth: number;
   pageHeight: number;
@@ -23,38 +21,35 @@ type PageProps = {
 export default function Page({ page, pageNumber, searchResults }: PageProps) {
   const imgRef = useRef<HTMLImageElement>(null);
   const [boxes, setBoxes] = useState(
-    [] as {
-      x: number;
-      y: number;
-      w: number;
-      h: number;
-    }[]
+    [] as { x: number; y: number; w: number; h: number }[]
   );
+
   useEffect(() => {
     if (imgRef.current && searchResults && searchResults.results) {
       const { width, height } = imgRef.current;
       const { pageWidth, pageHeight, results } = searchResults;
       setBoxes(
-        results.map(({ x, y, w, h }) => ({
-          x: (x / pageWidth) * width,
-          y: (y / pageHeight) * height,
-          w: (w / pageWidth) * width,
-          h: (h / pageHeight) * height,
+        results.map(({ bbox }) => ({
+          x: (bbox.x / pageWidth) * width,
+          y: (bbox.y / pageHeight) * height,
+          w: (bbox.w / pageWidth) * width,
+          h: (bbox.h / pageHeight) * height,
         }))
       );
     }
-  }, [searchResults]);
+  }, [searchResults, imgRef]);
 
-  if (boxes.length) {
-    return (
-      <div className="relative">
-        <Image
-          ref={imgRef}
-          src={page}
-          width={800}
-          height={800}
-          alt="PDF Page"
-        />
+  return (
+    <div key={pageNumber} className="relative">
+      <Image
+        ref={imgRef}
+        src={page}
+        layout="responsive"
+        width={searchResults?.pageWidth || 800}
+        height={searchResults?.pageHeight || 800}
+        alt="PDF Page"
+      />
+      {boxes.length > 0 && (
         <div className="absolute inset-0">
           {boxes.map(({ x, y, w, h }, key) => (
             <div
@@ -69,13 +64,7 @@ export default function Page({ page, pageNumber, searchResults }: PageProps) {
             />
           ))}
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div key={pageNumber} className="relative">
-      <Image ref={imgRef} src={page} width={800} height={800} alt="PDF Page" />
+      )}
     </div>
   );
 }

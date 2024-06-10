fa2aa1b2adc46f67833cf0e6348a375ddb3d0899
Alexander Martin <alex@xela.be>
2024-06-10 19:52:59 +0200

    test(): added some basic tests for rearrange and journal

--- examples/tests/src/metadata.test.ts
+++ examples/tests/src/metadata.test.ts
@@ -1,25 +1,28 @@
-import * as fs from 'fs';
-import * as mupdf from 'mupdf';
-import path from 'path';
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import * as fs from "fs";
+import * as mupdf from "mupdf";
+import path from "path";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 const scriptdir = path.resolve(__dirname);
 const filename = path.join(scriptdir, "resources", "001003ED.pdf");
 const metafile = path.join(scriptdir, "resources", "metadata.txt");
 
-describe('mupdfjs metadata tests', () => {
-    let document: mupdf.PDFDocument;
+describe("mupdfjs metadata tests", () => {
+  let document: mupdf.PDFDocument;
 
   beforeAll(async () => {
     const data = fs.readFileSync(filename);
-    document = await mupdf.Document.openDocument(data, 'application/pdf') as mupdf.PDFDocument;
+    document = (await mupdf.Document.openDocument(
+      data,
+      "application/pdf"
+    )) as mupdf.PDFDocument;
   });
 
   afterAll(() => {
     document.destroy();
   });
 
-  it('should match metadata with expected result', async () => {
+  it("should match metadata with expected result", async () => {
     const metadata = {
       format: document.getMetaData("format"),
       title: document.getMetaData("info:Title"),
@@ -31,14 +34,16 @@ describe('mupdfjs metadata tests', () => {
       creationDate: document.getMetaData("info:CreationDate"),
       modDate: document.getMetaData("info:ModDate"),
       trapped: document.getMetaData("info:Trapped"),
-      encryption: document.getMetaData("info:Encryption")
+      encryption: document.getMetaData("info:Encryption"),
     };
-  
-    const expectedMetadata = JSON.parse(fs.readFileSync(metafile, 'utf8'));
+
+    const expectedMetadata = JSON.parse(fs.readFileSync(metafile, "utf8"));
     // Convert undefined to an empty string
     function normalize(obj: any) {
-        return JSON.parse(JSON.stringify(obj, (key, value) => value === undefined ? '' : value));
+      return JSON.parse(
+        JSON.stringify(obj, (key, value) => (value === undefined ? "" : value))
+      );
     }
     expect(normalize(metadata)).toEqual(normalize(expectedMetadata));
   });
-})
+});
---
+++ examples/tests/src/rearrange.test.ts
@@ -0,0 +1,60 @@
+import * as fs from "fs";
+import * as mupdf from "mupdf";
+import path from "path";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+
+const scriptdir = path.resolve(__dirname);
+const filename = path.join(scriptdir, "..", "test.pdf");
+const data = fs.readFileSync(filename);
+
+describe("mupdfjs rearrangePages operation", () => {
+  let document: mupdf.PDFDocument;
+
+  beforeEach(async () => {
+    document = mupdf.Document.openDocument(
+      data,
+      "application/pdf"
+    ) as mupdf.PDFDocument;
+  });
+
+  afterEach(() => {
+    document.destroy();
+  });
+
+  it("should rearrange all pages in document", () => {
+    const structedTextFirstPage = document
+      .loadPage(0)
+      .toStructuredText()
+      .asJSON();
+    const structedTextSecondPage = document
+      .loadPage(1)
+      .toStructuredText()
+      .asJSON();
+    const structedTextThirdPage = document
+      .loadPage(2)
+      .toStructuredText()
+      .asJSON();
+
+    document.rearrangePages([2, 1, 0]);
+
+    expect(document.countPages()).toBe(3);
+    expect(document.loadPage(0).toStructuredText().asJSON()).toStrictEqual(
+      structedTextThirdPage
+    );
+    expect(document.loadPage(1).toStructuredText().asJSON()).toStrictEqual(
+      structedTextSecondPage
+    );
+    expect(document.loadPage(2).toStructuredText().asJSON()).toStrictEqual(
+      structedTextFirstPage
+    );
+  });
+
+  it("should rearrange some pages in document", () => {
+    document.rearrangePages([2, 1]);
+
+    expect(document.countPages()).toBe(2);
+    expect(document.loadPage(0)).toBeInstanceOf(mupdf.PDFPage);
+    expect(document.loadPage(1)).toBeInstanceOf(mupdf.PDFPage);
+    expect(() => document.loadPage(2)).toThrowError("invalid page number: 3");
+  });
+});

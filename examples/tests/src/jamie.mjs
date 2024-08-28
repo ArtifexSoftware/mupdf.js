cca3a1327fd85486291776c69f1e82ca56a680f2
Jamie Lemon <jamie.lemon@artifex.com>
2024-08-28 17:33:53 +0100

    More experimentation. Moves wrapper methods into tasks.ts.

--- examples/tests/src/jamie.mjs
+++ examples/tests/src/jamie.mjs
@@ -1,12 +1,13 @@
 import * as fs from "fs"
-import * as mupdf from "../../../dist/mupdf.js"
+import * as mupdfjs from "../../../dist/tasks.js"
 
 const filename = "../test.pdf"
 const file = fs.readFileSync(filename)
 
-let document = mupdf.PDFDocument.openDocument(file, "application/pdf")
+let document = mupdfjs.loadPDF(file)
+//let document = mupdfjs.PDFDocument.createBlankDocument()
 
-const page = document.loadPage(0)
-page.insertText("donkey kong",[10,400],"Times-Roman",50)
+let mupdfJSPage = new mupdfjs.PDFPage(document, document.loadPage(0))
+mupdfJSPage.insertText("donkey kong junior", [100,200], "Times-Roman", 40, {strokeColor:[0,0,0,0.2], fillColor:[0,1,0,0.75]})
 
 fs.writeFileSync("output.pdf", document.saveToBuffer("").asUint8Array())

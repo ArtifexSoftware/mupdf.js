74c7fbf8948b4dbae5ffb72913d8e2c46a7915e2
Jamie Lemon <jamie.lemon@artifex.com>
2024-08-29 17:48:15 +0100

    Starts the high level wrapper.
    This high level wrapper aims to provide
    many convenience methods and handy
    functionality taking inspiration from
    PyMuPDF.

---
+++ examples/tests/src/wrapper-test.mjs
@@ -0,0 +1,19 @@
+import * as fs from "fs"
+import * as tasks from "../../../dist/tasks.js"
+import * as mupdfjs from "../../../dist/mupdfjs.js"
+
+const filename = "../test.pdf"
+const file = fs.readFileSync(filename)
+
+let document = tasks.loadPDF(file)
+
+//let document = mupdfjs.PDFDocument.createBlankDocument()
+
+let mupdfJSPage = new mupdfjs.PDFPage(document, document.loadPage(0))
+mupdfJSPage.insertText("HELLO WORLD", 
+                       [100,200], 
+                       "Times-Roman", 
+                       50, 
+                       {strokeColor:[0,0,0,0.2], fillColor:[1,0,0,0.75]})
+
+fs.writeFileSync("output.pdf", document.saveToBuffer("").asUint8Array())
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,5 +1,29 @@
+// Copyright (C) 2024 Artifex Software, Inc.
+//
+// This file is part of MuPDF.js library.
+//
+// MuPDF is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// MuPDF is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with MuPDF. If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>
+//
+// Alternative licensing terms are available from the licensor.
+// For commercial licensing, see <https://www.artifex.com/> or contact
+// Artifex Software, Inc., 39 Mesa Street, Suite 108A, San Francisco,
+// CA 94129, USA, for further information.
+
 import * as mupdf from "mupdf"
 
+// convenience methods for the mupdf.js migration
+// see: https://mupdfjs.readthedocs.io/en/latest/how-to-guide/migration/index.html
 export function loadPDF(data: Buffer | ArrayBuffer | Uint8Array) {
     return new mupdf.PDFDocument(data)
 }

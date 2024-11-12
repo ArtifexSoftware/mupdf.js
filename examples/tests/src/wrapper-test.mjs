a9aa2a239166424e6eb35ddda30e9a474a9f6576
Jamie Lemon <jamie.lemon@artifex.com>
2024-11-12 13:52:39 +0000

    Corrects load page error in wrapper-test.mjs.

--- examples/tests/src/wrapper-test.mjs
+++ examples/tests/src/wrapper-test.mjs
@@ -1,15 +1,12 @@
 import * as fs from "fs"
-import * as tasks from "../../../dist/tasks.js"
 import * as mupdfjs from "../../../dist/mupdfjs.js"
 
 const filename = "../test.pdf"
 const file = fs.readFileSync(filename)
 
-//let document = tasks.loadPDF(file)
-
 let document = mupdfjs.PDFDocument.createBlankDocument()
 
-let mupdfJSPage = new mupdfjs.PDFPage(document, document.loadPage(0))
+let mupdfJSPage = new mupdfjs.PDFPage(document, 0)
 mupdfJSPage.insertText("HELLO WORLD", 
                        [100,200], 
                        "Times-Roman", 
@@ -21,6 +18,7 @@ let image = new mupdfjs.Image(fs.readFileSync("../logo.png"))
 mupdfJSPage.insertImage({image:image, name:"MyLogo"}, {x:200, y:300})
 
 fs.writeFileSync("output.pdf", document.saveToBuffer("").asUint8Array())
+
 
 
 

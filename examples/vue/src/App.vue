4ecb86022ea2bccc2e114b932de0b72b8c5f4f93
Jason Athanasoglou <jathanasoglou@outlook.com>
2025-01-23 01:40:49 +0200

    Format App.vue

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -2,21 +2,21 @@
 import { useMupdf } from '@/composables/useMupdf';
 import { ref, watch } from 'vue';
 
-const { workerInitialized, loadDocument, renderPage } = useMupdf()
-const pdfUrl = ref<string | null>(null)
+const { workerInitialized, loadDocument, renderPage } = useMupdf();
+const pdfUrl = ref<string | null>(null);
 
 // ===> This is a demo callback which uses functions <===
 // ===> from useMupdf to load and display the first page <===
 // ===> of the pdf as an image. <===
 watch(workerInitialized, async (isInitialized) => {
   if (isInitialized) {
-    const response = await fetch('/test.pdf')
-    const arrayBuffer = await response.arrayBuffer()
-    await loadDocument(arrayBuffer)
-    const pngData = await renderPage(0)
-    pdfUrl.value = URL.createObjectURL(new Blob([pngData], { type: 'image/png' }))
+    const response = await fetch('/test.pdf');
+    const arrayBuffer = await response.arrayBuffer();
+    await loadDocument(arrayBuffer);
+    const pngData = await renderPage(0);
+    pdfUrl.value = URL.createObjectURL(new Blob([pngData], { type: 'image/png' }));
   }
-})
+});
 </script>
 
 <template>
